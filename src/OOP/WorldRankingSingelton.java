a616892c2bd7224b04a7886da4215b7ad4dbe7fc
lizag <lizagilman@gmail.com>
2016-12-04 01:48:29 +0200

    stuff

--- src/OOP/WorldRankingSingelton.java
+++ src/OOP/WorldRankingSingelton.java
@@ -5,6 +5,7 @@ public class WorldRankingSingelton {
     private static WorldRankingSingelton wrInstance = new WorldRankingSingelton();
 
     public static WorldRankingSingelton getInstance() {
+
         return wrInstance;
     }
 
--- src/liza/MyMain.java
+++ src/liza/MyMain.java
@@ -48,7 +48,5 @@ public class MyMain {
 //        myvec.add(liza);
 //        myvec.add(jan);
 
-
-
     }
 }
